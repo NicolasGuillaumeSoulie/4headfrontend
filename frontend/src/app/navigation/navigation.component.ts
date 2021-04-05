b6d606f40be478db6577f65668577f399e4cd07b
Victor DALIBOT <victor@efrei.net>
2021-04-05 15:24:03 -0700

    je ne sais plus

--- frontend/src/app/navigation/navigation.component.ts
+++ frontend/src/app/navigation/navigation.component.ts
@@ -5,4 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent{}
+export class NavigationComponent{
+
+  
+}
